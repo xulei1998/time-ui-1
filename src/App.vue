75d3c8e5d349e50370c329b60a04c02561da5d5a
berthahsu <271181958@qq.com>
2022-03-05 13:39:55 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>  <!---->
+<div>hi</div>
+ <Lucy/>
+</template>
+
+<script>
+import Lucy from './components/Lucy.vue'  //从这里引入的Lucy组件
+
+export default {
+  name: 'App',
+  components: {
+    Lucy:Lucy
+  }
+}
+</script>
